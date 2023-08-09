4af7528b5a01415e152a6da6c3f62e645d9713dd
Andriy Amelin <andrew.amelin@gmail.com>
2023-08-09 23:02:36 +0200

    EmployeeRepository: added delete by id method

--- src/main/java/company/employee/repository/EmployeeRepository.java
+++ src/main/java/company/employee/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package company.employee.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import company.employee.domain.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+    long deleteByEmployeeId(UUID employId);
 }
