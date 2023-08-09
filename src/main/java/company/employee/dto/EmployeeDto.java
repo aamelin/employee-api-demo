b0572092b43f83a3793af2c1f1ebde54088e3c99
Andriy Amelin <andrew.amelin@gmail.com>
2023-08-09 23:02:03 +0200

    EmployeeDto constraints added

--- src/main/java/company/employee/dto/EmployeeDto.java
+++ src/main/java/company/employee/dto/EmployeeDto.java
@@ -2,15 +2,21 @@ package company.employee.dto;
 
 import java.time.LocalDate;
 import java.util.Set;
+import java.util.UUID;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class EmployeeDto {
+    @NotNull
+    @org.hibernate.validator.constraints.UUID
+    private UUID employeeId;
     @NotBlank
     private String firstName;
     @NotBlank
